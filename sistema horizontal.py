6e1fea48f5498a3f6dc459a601da9e872e2ad5d7
oscarbarra <obarra2023@alu.uct.cl>
2023-06-28 13:08:47 -0400

    dato por definicion

--- sistema horizontal.py
+++ sistema horizontal.py
@@ -1,80 +1,145 @@
 #abrir como carpeta
 #definicion de las constantes 
 #modificar este codigo y no el MAIN
-P1 = 0; P2 = 0; M1 = 0; M2 = 0; T = 0; A = 0 ; F = 0; complemento = ''; repeticion = ''
+#python 3.11.4
 
-#creacion de los modelos
-def aceleracion(F,M1,M2):
+import math
+
+P1 = 0; P2 = 0; M1 = 0; M2 = 0; F = 0 ; g = 9.8; 
+A = 0; T = 0; aceleracionPI = 0; tensionPI = 0; aceleracionPI2 = 0
+angulo_inclinacion_1 = 0;angulo_inclinacion_2 = 0
+complemento = ''
+
+#---------------------------------------------------------------------------
+#---------------------------ingtreso de datos-------------------------------
+#---------------------------------------------------------------------------
+def peso_1():
+    global P1
+    P1 = float(input("valor del peso 1: "))
+    return
+
+def peso_2():
+    global P2
+    P2 = float(input("valor del peso 2: "))
+    return
+
+def masa_1():
+    global M1
+    if P1 != 0:
+        M1 = P1 / g
+    else:
+        M1 = float(input("valor de la masa 1: "))
+    return
+
+def masa_2():
+    global M2
+    if P2 != 0:
+        M2 = P1 / g
+    else:
+        M2 = float(input("valor de la masa 2: "))
+
+def fuerza_aplicada():
+    global F
+    F = float(input("valor de la fueraz aplicada: "))
+
+def anglulo_inclinacion_A():
+    global angulo_inclinacion_1
+    angulo_inclinacion_1 = float(input("Ingrese el ángulo de inclinación 1 (en grados): "))
+    return
+
+def angulo_inclinacion_B():
+    global angulo_inclinacion_2
+    angulo_inclinacion_2 = float(input("Ingrese el ángulo de inclinación 2 (en grados): "))
+#---------------------------------------------------------------------------
+# Sistema de dos masas superficie horizontal con una fuerza externa aplicada
+#---------------------------------------------------------------------------
+ # Calcula la aceleración 
+def aceleracion():
     global A
     A = (F/(M1+M2))
-    return A
-
-def tension(F,M1,M2):
+    return
+# Calcula la tensión en la cuerda que conecta las masas
+def tension():
     global T
-    T = (M2*(F/(M1+M2)))
-    return T
+    T = M1 * A
+    return 
+#--------------------------------------------------
+# Sistema de dos masas superficie inclinada
+# CASO 1 (UN SOLO ANGULO)
+#--------------------------------------------------
+# Cálculo de la aceleración en la superficie inclinada
+def aceleracion_1Angulo():
+    global aceleracionPI
+    aceleracionPI = ((M2 * g) - ((M1 * g * math.sin(math.radians(angulo_inclinacion_1))) / (M1 + M2 )))
+    return
 
-#complemento
+# Cálculo de la tensión en la cuerda o superficie inclinada
+def tension_1Angulo():
+    global tensionPI
+    tensionPI = ((M2 * g) - (M1 * aceleracionPI))
+    return 
+
+#--------------------------------------------------
+# Sistema de dos masas superficie inclinada
+#CASO 2 (DOS ANGULOS)
+#--------------------------------------------------
+# Cálculo de la aceleración en la superficie inclinada con dos angulos
+def aceleracion_2Angulos():
+    global aceleracionPI2
+    aceleracionPI2 = (M2 * g * math.sin(math.radians(angulo_inclinacion_2))) - ((M1 * g * math.sin(math.radians(angulo_inclinacion_1)) ) / (M1 + M2))
+    return 
+
+# Cálculo de la tensión en la superficie inclinada con dos angulos
+def tension_2Angulos():
+    global tensionPI2
+    tensionPI2 = ((M1 * aceleracionPI2) + (M1* g * math.sin(math.radians(angulo_inclinacion_1))))
+    return 
+#----------------------------------------------------------------------------
+
+#---------complemento----------------------
 def complemento_0():
     global complemento
     complemento = input("otra cosita?: ")
     return
 
-#repeticion
-def repeticion_0():
-    global repeticion
-    repeticion = input("quieres otros valores?: ")
-    return
-
-#historial
-
-def relleno():
-    with open('historial.txt','r+') as h:
-        h.readlines()
-        h.write(("M1 = ")+str(M1)+("\n")+("M2 = ")+str(M2)+("\n"))
-        h.write(("fuerza aplicada = ")+(str(F))+("\n"))
-        h.write(("A = ")+str(A)+'\n'+("T = ")+str(T)+("\n"))
-        h.write("------------------------------------------------"+("\n"))
-
-
-#INGRESO DE LOS DATOS
-def datos():
-    global P1,P2,M1,M2,F
-    P1 = float(input("peso objeto 1: "))
-    P2 = float(input("peso objeto 2: "))
-    F = float(input("fuerza aplicada: "))
-    if P1 != 0:
-        M1 = P1/9.8
-    else:
-        M1 = float(input("masa objeto 1: "))
-    if P2 != 0:
-        M2 = P2/9.8
-    else:
-        M2 = float(input("masa objeto 2: "))
-    return
-
 #progrma principal
-datos()
-h = open("historial.txt","w")
-h.close
 while True:
-    encontrar = input("que desas conocer?: ")
+    encontrar = input("que deseas conocer?: ")
+    #-----------------------sitema horizontal--------------------------------
     if encontrar == 'aceleracion':
-        print("a = ",aceleracion(F,M1,M2))
-    elif encontrar == 'tension':
-        print("t = ",tension(F,M1,M2))
-    while True:
-        complemento_0()
+        peso_1(); peso_2(); masa_1(); masa_2(); fuerza_aplicada() #datos
+        aceleracion()
+        print("aceleracion = ",A)
+    elif encontrar == 'tension': 
+        peso_1(); peso_2(); masa_1(); masa_2(); fuerza_aplicada() #datos
+        aceleracion(); tension()
+        print("tension = ",T)
+    #-------------------------plano inclinado con 1 angulo----------------------------
+    elif encontrar == 'tension caso 1':
+        peso_1(); peso_2(); masa_1(); masa_2(); anglulo_inclinacion_A() #datos
+        aceleracion_1Angulo()
+        tension_1Angulo()
+        print("tension de la superficie inclinada = ",tensionPI)
+    elif encontrar == 'aceleracion caso 1':
+        peso_1(); peso_2(); masa_1(); masa_2(); anglulo_inclinacion_A() #datos
+        aceleracion_1Angulo()
+        print("aceleracion de la superficie inclinada = ",aceleracionPI)
+    #-----------------plano inclinado con 2 angulos-----------------------------------
+    elif encontrar == 'tension caso 2':
+        peso_1(); peso_2(); masa_1(); masa_2(); anglulo_inclinacion_A(); angulo_inclinacion_B() #datos
+        aceleracion_2Angulos()
+        tension_2Angulos()
+        print("tension de la superficie inclinada dos angulos = ",tensionPI2)
+    elif encontrar == 'aceleracion caso 2':
+        peso_1(); peso_2(); masa_1(); masa_2(); anglulo_inclinacion_A(); angulo_inclinacion_B() #datos
+        aceleracion_2Angulos()
+        print("aceleracion de la superficie inclinada 2 angulos = ",aceleracionPI2)
+        #-------------------------------------------------------------------------------
+        #complemento_0()
         if complemento == 'si':
             if encontrar == 'aceleracion':
-                print("t = ",tension(F,M1,M2))
+                print("t = ",tension())
             elif encontrar == 'tension':
-                print("a = ",aceleracion(F,M1,M2))
+                print("a = ",aceleracion())
         elif complemento == 'no':
             break
-        relleno()
-    repeticion_0()
-    if repeticion == 'si':
-        datos()
-    elif repeticion == 'no':
-        break
